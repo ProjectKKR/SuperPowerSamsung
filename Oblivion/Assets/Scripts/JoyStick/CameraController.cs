247e21f72c91022cf9655b1008f23ea466fd00bc
rhs0266 <dkflfkdeh@naver.com>
2016-04-25 15:03:16 +0900

    Base Project

---
+++ Oblivion/Assets/Scripts/JoyStick/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+	public VirtualJoystick_left jsl;
+	public GameObject Player;
+
+	private Vector3 pos;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		pos = Player.transform.position;
+		pos.z += 0.5F;
+		gameObject.transform.position = pos;
+		//gameObject.transform.Rotate (new Vector3(0,jsl.Turn (),0));
+	}
+}
---
+++ Oblivion/Assets/Scripts/JoyStick/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public VirtualJoystick_left jsL;
+	public VirtualJoystick_right jsR;
+	public float moveSpeed;
+	public float terminalRotationSpeed = 25.0f;
+	public Camera mainCamera;
+
+	private Vector3 MoveVector;
+	private Rigidbody rb;
+	// Use this for initialization
+
+	void Start () {
+		rb = gameObject.GetComponent<Rigidbody> ();
+		rb.maxAngularVelocity = terminalRotationSpeed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		MoveVector = PoolRightInput ();
+		Vector3 v = MoveVector * moveSpeed;
+		rb.AddRelativeForce (v*20);
+		float norm = rb.velocity.magnitude;
+		if (rb.velocity.magnitude > 0.8f*norm)
+			rb.velocity = rb.velocity.normalized * 0.8f * norm;
+		if (MoveVector == Vector3.zero)
+			rb.velocity = Vector3.zero;
+
+		gameObject.transform.Rotate (new Vector3(0,jsR.Turn ()*1.3f,0));
+	}
+
+	private Vector3 PoolRightInput(){
+		Vector3 dir = Vector3.zero;
+		dir.x = jsL.Horizontal ();
+		dir.z = jsL.Vertical ();
+		if (dir.magnitude > 1)
+			dir.Normalize ();
+		return dir;
+	}
+}
