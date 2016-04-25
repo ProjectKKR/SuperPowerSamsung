129ab27a8acd7d7497cb722dee956328e25ff776
unknown <>
2016-04-25 15:33:54 +0900

    game item scripts start

---
+++ Oblivion/Assets/Scripts/GameItems.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class GameItems : MonoBehaviour {
+	bool interactable;
+	bool zoomable;
+	bool visible;
+
+	protected Vector3 zoomCameraLocation;
+	public Vector3 ZoomCameraLocation {
+		get {
+			return zoomCameraLocation;
+		}
+	}
+	protected float distanceThreshold;
+	public float DistanceThreshold {
+		get {
+			return distanceThreshold;
+		}
+	}
+
+	void get () {
+		// TODO compare distance, then call ClickInteraction method
+	}
+	public abstract void ClickInteraction ();
+}
---
+++ Oblivion/Assets/Scripts/Umbrella.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Umbrella : GameItems {
+	
+	// Use this for initialization
+	void Start () {
+
+		// TODO example
+		distanceThreshold = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ClickInteractive() {
+		// TODO
+	}
+}
